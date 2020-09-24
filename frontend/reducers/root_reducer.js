6a9fdb318e525410cdf2a179a550174403d08b0e
Ciro <Ciro16@gmail.com>
2020-09-23 19:23:39 -0600

    created Todo App

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import todosReducer from './todos_reducer'
+
+const rootReducer = combineReducers({todos: todosReducer})
+{
+    todos: {
+        
+    }
+}
+export default rootReducer
